f581444d16e32897fdad0e7f8d6c7a5934f815d8
ErJump <fedelijunior@gmail.com>
2023-02-08 10:58:24 +0100

    feat: :sparkles: Adds counter with increment and decrement feature (no vuex yet)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="home">
-    <h1>Hello World!</h1>
+  <div class="home container-lg py-5">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1 class="mb-4">VueX Counter</h1>
+        <p>Click the buttons to increment and decrement the counter.</p>
+        <h2 class="ms_counter mb-4">
+          {{ counter }}
+        </h2>
+        <button class="btn btn-danger me-4" @click="decrement()">-</button>
+        <button class="btn btn-primary" @click="increment()">+</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,5 +18,18 @@
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
+  }
 }
 </script>
